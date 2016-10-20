7d044e3f883a2a6c2227715a1a7ee5bc3271f573
JoyceMatos <j.matosjoyce@gmail.com>
2016-10-20 17:58:16 -0400

    Lab Completed

--- Hospital/SchoolEmployees.swift
+++ Hospital/SchoolEmployees.swift
@@ -3,9 +3,28 @@
 //  Hospital
 
 
+struct AdvisoryBoardMember: Employee {
+    var name: String
+    var vacationDays: Int = 30
+    
+    func wages() -> Double {
+        return 100_000.00
+    }
+}
 
+struct Principal: Employee, Reprimand {
+    var name: String
+    var vacationDays: Int = 20
+    
+    func wages() -> Double {
+        return 80_000.00
+    }
+}
 
-
+struct Teacher: Employee, Teach {
+    var name: String
+    var vacationDays: Int = 15
+}
 
 
 
